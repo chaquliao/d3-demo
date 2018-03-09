09fc9f340db409e9dec9e0b9a5d366117458b969
LiaoFangzhengqing <fangzheng-qing.liao@hpe.com>
2018-03-09 17:53:20 +0800

    update pie chart

--- src/views/pieChart.vue
+++ src/views/pieChart.vue
@@ -6,20 +6,78 @@ import * as d3 from "d3";
 
 export default {
   mounted: function() {
-  	this.initChart()
+    this.initChart()
   },
 
   methods: {
     initChart() {
-      var svg_width = 400,
-        svg_height = 400,
-        data = [10, 20, 5, 9, 14];
+      var svg_width = 500,
+        svg_height = 500,
+        data = [{
+          value: 1,
+          label: "rass",
+          fill: "steelblue"
+        }, {
+          value: 5,
+          label: "core",
+          fill: "#C1CDCD"
+        }, {
+          value: 2,
+          label: "leap",
+          fill: "#FFD39B"
+        }, {
+          value: 2,
+          label: "chongqing",
+          fill: "#FF7256"
+        }, {
+          value: 8,
+          label: "chengdu",
+          fill: "#9BCD9B"
+        }, {
+          value: 12,
+          label: "hangzhou",
+          fill: "steelblue"
+        }];
+      var values = data.map(item => {
+        return item.value
+      });
+      var fills = data.map(item => {
+        return item.fill
+      });
 
-      d3.select("#container")
-      .append("svg")
-      .attr("width",svg_width)
-      .attr("height",svg_height)
-      .attr("transform",`translate(${svg_width/2},${svg_height/2})`)
+      var pie = d3.pie();
+      var pieData = pie(values)
+      console.log(pieData)
+      var outerRadius = 150; //外半径
+      var innerRadius = 50; //内半径，为0则中间没有空白
+
+      var arc = d3.arc() //弧生成器
+        .innerRadius(innerRadius) //设置内半径
+        .outerRadius(outerRadius); //设置外半径
+
+      // var color = d3.scale.category20();
+
+      var svg = d3.select("#container")
+        .append("svg")
+        .attr("width", svg_width)
+        .attr("height", svg_height)
+
+      var arcs = svg.selectAll("g")
+        .data(pieData)
+        .enter()
+        .append("g")
+        .attr("transform", `translate(${svg_width/2},${svg_height/2})`)
+
+      arcs.append("path")
+        .attr("fill", function(d,i){
+        	return fills[i]
+        })
+        .attr("d", function(d) {
+          return arc(d);
+        });
+
+
+
     }
   }
 }
